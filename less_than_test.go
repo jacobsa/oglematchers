f868c4955a81da39774c6a90d47505c469d375d6
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-12-19 15:45:41 +1100

    Added a test skeleton for LessThan.

---
+++ less_than_test.go
@@ -0,0 +1,56 @@
+// Copyright 2011 Aaron Jacobs. All Rights Reserved.
+// Author: aaronjjacobs@gmail.com (Aaron Jacobs)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ogletest
+
+import (
+	"testing"
+)
+
+////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////
+
+type ltTestCase struct {
+	candidate      interface{}
+	expectedResult MatchResult
+	expectedError  string
+}
+
+func checkLtTestCases(t *testing.T, matcher Matcher, cases []ltTestCase) {
+	for i, c := range cases {
+		result, err := matcher.Matches(c.candidate)
+
+		if result != c.expectedResult {
+			t.Errorf(
+				"Case %d (candidate %v): expected %v, got %v",
+				i,
+				c.candidate,
+				c.expectedResult,
+				result)
+		}
+
+		if err != c.expectedError {
+			t.Errorf("Case %d: expected error %v, got %v", i, c.expectedError, err)
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////
+// Integer literals
+////////////////////////////////////////////////////////////
+
+func TestFoo(t *testing.T) {
+}
