79baed24b33b7c80b13257ebd1a3ece36e04c396
Aaron Jacobs <aaronjjacobs@gmail.com>
2011-12-19 11:36:45 +1100

    Trashed initial implementation.

--- equals.go
+++ equals.go
@@ -17,7 +17,6 @@ package ogletest
 
 import (
 	"fmt"
-	"reflect"
 )
 
 // Equals returns a matcher that matches any value v such that v == x, with the
@@ -31,75 +30,15 @@ type equalsMatcher struct {
 	expected interface{}
 }
 
-func isSignedInteger(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return true
-	}
+////////////////////////////////////////////////////////////
+// Numeric types
+////////////////////////////////////////////////////////////
 
-	return false
-}
-
-func isUnsignedInteger(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return true
-	}
-
-	return false
-}
-
-func isFloat(v reflect.Value) bool {
-	k := v.Kind()
-	return k == reflect.Float32 || k == reflect.Float64
-}
-
-func isComplex(v reflect.Value) bool {
-	k := v.Kind()
-	return k == reflect.Complex64 || k == reflect.Complex128
-}
-
-func checkAgainstInt(e int64, v reflect.Value) (res MatchResult, err string) {
-	res = MATCH_FALSE
-
-	switch {
-	case isSignedInteger(v):
-		if e == v.Int() {
-			res = MATCH_TRUE
-		}
-
-	case isUnsignedInteger(v):
-		if e >= 0 && uint64(e) == v.Uint() {
-			res = MATCH_TRUE
-		}
-
-	case isFloat(v):
-		if float64(e) == v.Float() {
-			res = MATCH_TRUE
-		}
-
-	case isComplex(v):
-		if complex(float64(e), 0) == v.Complex() {
-			res = MATCH_TRUE
-		}
-
-	default:
-		res = MATCH_UNDEFINED
-		err = "which is not numeric"
-	}
-
-	return
-}
+////////////////////////////////////////////////////////////
+// Public implementation
+////////////////////////////////////////////////////////////
 
 func (m *equalsMatcher) Matches(candidate interface{}) (MatchResult, string) {
-	expectedValue := reflect.ValueOf(m.expected)
-	candidateValue := reflect.ValueOf(candidate)
-
-	switch {
-	case isSignedInteger(expectedValue):
-		return checkAgainstInt(expectedValue.Int(), candidateValue)
-	}
-
 	return MATCH_UNDEFINED, "TODO"
 }
 
